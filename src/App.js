133dcf48fbf1377c5a0cebeee3de2f90b1283432
elzup <guild0105@gmail.com>
2017-04-28 00:21:49 +0900

    Moment loop

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
+import moment from 'moment'
 import StoreBoard from './StoreBoard';
+
+const data = {
+	stores: [
+		{ name: "松屋", start: '00:00', end: '24:00', },
+		{ name: "スーパーたなか", start: '09:00', end: '23:00', },
+		{ name: "武蔵屋", start: '11:00', end: '01:00', },
+	]
+};
 
 class App extends Component {
 	render() {
+		const storeBoards = [];
+		data.stores.forEach((e) => {
+			storeBoards.push(
+				<StoreBoard
+					key={e.name}
+					name={e.name}
+					start={moment(e.start, 'HH:mm')}
+					end={moment(e.end, 'HH:mm')}
+				/>
+			);
+		});
+
 		return (
 			<div className="App">
 				<div className="App-header">
@@ -12,21 +33,7 @@ class App extends Component {
 					<p>飲食店開店時間情報</p>
 				</div>
 				<div style={{ display: 'flex' }}>
-					<StoreBoard
-						name="松屋"
-						start="9時"
-						end="20時"
-					/>
-					<StoreBoard
-						name="武蔵屋"
-						start="9時"
-						end="20時"
-					/>
-					<StoreBoard
-						name="5号館"
-						start="9時"
-						end="20時"
-					/>
+					{ storeBoards }
 				</div>
 			</div>
 		);
--- src/StoreBoard.js
+++ src/StoreBoard.js
@@ -1,22 +1,41 @@
 import React from 'react';
 import './App.css';
+import moment from 'moment'
+
+moment.locale("ja",
+	{
+		relativeTime: {
+			future: 'あと%s',
+			past: '%s前',
+			s: '秒',
+			m: '1分',
+			mm: '%d分',
+			h: '1時間',
+			hh: '%d時間',
+			d: 'un日',
+			dd: '%d日',
+		}
+	});
+
 
 class StoreBoard extends React.Component {
 	render() {
 		console.log(this.props);
 		const { name, start, end } = this.props;
+		const format = 'HH時mm分';
+		const isClose = start < moment.now;
 		return (
-			<div className="Store" style={{ width: "200px", margin: "20px"}}>
+			<div className="Store" style={{
+				width: "200px",
+				margin: "20px",
+				background: isClose ? 'red' : '#aaa'
+			}}>
 				<h2>{ name }</h2>
-				<p>{ start } - { end }</p>
-				<p></p>
+				<p>開店まで { start.fromNow() }</p>
+				<p>{ start.format(format) } - { end.format(format) }</p>
 			</div>
 		);
 	}
 }
-
-StoreBoard.propTypes = {
-	name: React.PropTypes.string
-};
 
 export default StoreBoard;
