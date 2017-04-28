8e0118dec6a5f00759371d283f5382127baccdcd
elzup <guild0105@gmail.com>
2017-04-28 22:09:26 +0900

    Fix today's day bug

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import moment from 'moment'
-import StoreBoard from './StoreBoard';
+import StoreBoard from './components/StoreBoard';
 import axios from 'axios';
 import ScheduleParser from './ScheduleParser'
 
--- src/components/StoreBoard.js
+++ src/components/StoreBoard.js
@@ -2,14 +2,14 @@ import React from 'react';
 import moment from 'moment'
 import _ from 'lodash';
 
-import { weekNames } from './ScheduleParser'
+import { weekNames } from '../ScheduleParser'
 
 moment.locale("ja",
 	{
 		relativeTime: {
 			future: 'あと%s',
 			past: '%s前',
-			s: '秒',
+			s: '%d秒',
 			m: '1分',
 			mm: '%d分',
 			h: '1時間',
@@ -28,17 +28,19 @@ class StoreBoard extends React.Component {
 
 		const today = schedules[_.keys(weekNames)[now.weekday]] || schedules['base'];
 		let isClose = false;
-		let next = today[0].start;
+		let next = today[0].start.clone().add({d: 1});
 		_.each(today, (term) => {
 			if (now <= term.start) {
 				isClose = true;
 				next = term.start;
 				return false;
 			}
-			if (term.start < now || now < term.end) {
+			if (term.start < now && now < term.end) {
+				isClose = false;
 				next = term.end;
 				return false;
 			}
+			isClose = true;
 		});
 
 		this.state = {
@@ -88,8 +90,8 @@ class StoreBoard extends React.Component {
 			}}>
 				<span>{ isClose ? "閉店" : "開店" }</span>
 				<h2>{ name }</h2>
-				{ this.renderNextChange() }
 				<p>{category}</p>
+				{ this.renderNextChange() }
 				<ul>
 					{days}
 				</ul>
